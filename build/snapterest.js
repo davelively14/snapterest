262a5ef808289a24bfc587568a1ac8c27b0cbe2c
Dave Lively <davelively14@gmail.com>
2016-07-14 10:07:22 -0400

    Added section

--- build/snapterest.js
+++ build/snapterest.js
@@ -20424,7 +20424,11 @@ module.exports = require('./lib/React');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var reactElement = React.createElement('h1', { className: 'header' });
-ReactDOM.render(reactElement, document.getElementById('react-application'));
+var h1 = React.createElement('h1', { className: 'header', key: 'header' }, 'This is React');
+var p = React.createElement('p', { className: 'content', key: 'content' }, "And this is how it works.");
+var reactFragment = [h1, p];
+var section = React.createElement('section', { className: 'container' }, reactFragment);
+
+ReactDOM.render(section, document.getElementById('react-application'));
 
 },{"react":171,"react-dom":28}]},{},[172]);
