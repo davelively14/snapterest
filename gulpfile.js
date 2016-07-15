5ab6e2d4b5bde93df54f0310586b3a07fab20a08
Dave Lively <davelively14@gmail.com>
2016-07-15 08:27:29 -0600

    Made component, renamed to app.jsx

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
 gulp.task('default', function() {
-  return browserify('./source/app.js')
+  return browserify('./source/app.jsx')
          .transform(babelify.configure({ presets: ["es2015", "react"] }))
          .bundle()
          .pipe(source('snapterest.js'))
