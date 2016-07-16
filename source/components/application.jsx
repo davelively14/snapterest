13fc3ab77d1a0bcdb9e7f1cedca7c6d655985930
Dave Lively <davelively14@gmail.com>
2016-07-16 09:55:17 -0600

    Added documentation

--- source/components/application.jsx
+++ source/components/application.jsx
@@ -1,14 +1,26 @@
+// Importing CommonJS dependency modules.
 var React = require('react')
-var Stream = require('./stream.jsx')
-var Collection = require('./collection.jsx')
+var Stream = require('./stream')
+var Collection = require('./collection')
 
+// Define React components. React specific.
 var Application = React.createClass({
 
+  // React API
   getInitialState: function () {
     return {
       collectionTweets: {}
     }
   },
+
+  // Notes on addTweetToCollection, removeTweetFromCollection,
+  // removeAllTweetsFromCollection:
+  // These callback functions will be passed to child componenets as a property.
+  // When a child component wants to update the state, it calls the callback
+  // function and passes necessary data. This parent component will update the
+  // state and then trigger the render() function that re-renders all the child
+  // components as necessary. This lets the child component focus on how to
+  // render itself and allows the parent to control its own state.
 
   addTweetToCollection: function (tweet) {
     var collectionTweets = this.state.collectionTweets
@@ -36,6 +48,7 @@ var Application = React.createClass({
     })
   },
 
+  // React API
   render: function () {
     return (
       <div className="container-fluid">
@@ -52,4 +65,5 @@ var Application = React.createClass({
   }
 })
 
+// Exporting the React component as a CommonJS module.
 module.exports = Application
